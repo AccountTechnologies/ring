616f528bdbe7905a3082f6afa9309c00761bf3da
queil <queil@users.noreply.github.com>
2021-11-11 08:40:45 +0000

    Update Assembly Info

--- src/source/ATech.Ring.Vsix17/Properties/AssemblyInfo.cs
+++ src/source/ATech.Ring.Vsix17/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ATech.Ring.Vsix17")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Account Technologies Software")]
 [assembly: AssemblyProduct("ATech.Ring.Vsix17")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Account Technologies Software 2018 - 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
