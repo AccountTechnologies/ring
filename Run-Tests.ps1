fa7b535013c229a0fffc26a3b340087f5595110c
queil <queil@users.noreply.github.com>
2021-12-12 23:49:15 +0000

    Simplify test run

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -1,13 +1,3 @@
 Install-Module -Name Pester -Force
 Import-Module -Name Pester
-
-
-switch ($true) {
-  $env:TF_BUILD {
-    Invoke-Pester -Path "./Ring.test.ps1" -CI -OutputFile "./Test-Pester.XML"
-  }
-  default {
-    $Container = New-PesterContainer -Path "$PSScriptRoot/Ring.test.ps1"
-    Invoke-Pester -Container $Container -Output Detailed
-  }
-}
+Invoke-Pester -CI
