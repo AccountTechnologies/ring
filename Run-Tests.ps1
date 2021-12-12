4b9250002c66319ca00cc60b4d1b2d1eac3fbea6
queil <queil@users.noreply.github.com>
2021-12-12 23:14:03 +0000

    Azure vars

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -4,7 +4,7 @@ Import-Module -Name Pester
 
 switch ($true) {
   $env:TF_BUILD {
-    Invoke-Pester -Script $(System.DefaultWorkingDirectory)\Ring.test.ps1 -OutputFile $(System.DefaultWorkingDirectory)\Test-Pester.XML -OutputFormat NUnitXML
+    Invoke-Pester -Script "$(System.DefaultWorkingDirectory)\Ring.test.ps1" -OutputFile "$(System.DefaultWorkingDirectory)\Test-Pester.XML" -OutputFormat NUnitXML
   }
   default {
     $Container = New-PesterContainer -Path "$PSScriptRoot/Ring.test.ps1"
