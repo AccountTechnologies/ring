1069d5df37ffaaa7d629b1ce02167ed220b565e7
queil <queil@users.noreply.github.com>
2022-01-03 12:02:47 +0000

    Avoid throwing when ws is not open

--- src/ATech.Ring.Protocol.v2/WebSocketExtensions.cs
+++ src/ATech.Ring.Protocol.v2/WebSocketExtensions.cs
@@ -7,12 +7,15 @@ using System.Threading.Tasks;
 namespace ATech.Ring.Protocol.v2;
 public static class WebSocketExtensions
 {
-
     public static async Task SendAckAsync(this WebSocket s, Ack status, CancellationToken token = default)
-        => await s.SendAsync(new byte[] { (byte)status }, WebSocketMessageType.Binary, true, token).ConfigureAwait(false);
+    {
+        if (s.State != WebSocketState.Open) return;
+        await s.SendAsync(new byte[] { (byte)status }, WebSocketMessageType.Binary, true, token).ConfigureAwait(false);
+    }
 
     public static Task SendMessageAsync(this WebSocket s, Message m, CancellationToken token = default)
     {
+        if (s.State != WebSocketState.Open) return Task.CompletedTask;
         return s.SendAsync(new ArraySegment<byte>(m.Bytes.ToArray()), WebSocketMessageType.Binary, true, token);
     }
 
